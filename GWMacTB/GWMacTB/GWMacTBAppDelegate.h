7ef3675c0351169d2a0db31ecd76ea710cd34fa5
gujinyou <gujinyou@115.com>
2013-12-28 11:39:21 +0800

    创建工程

---
+++ GWMacTB/GWMacTB/GWMacTBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GWMacTBAppDelegate.h
+//  GWMacTB
+//
+//  Created by 115 on 13-12-28.
+//  Copyright (c) 2013年 xiaoye. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GWMacTBAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ GWMacTB/GWMacTB/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  GWMacTB
+//
+//  Created by 115 on 13-12-28.
+//  Copyright (c) 2013年 xiaoye. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
